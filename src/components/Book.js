5e631fb3fb2481ab59e3d1b669585d2d317ebc42
Nezerwa <eligrand2000@gmail.com>
2023-03-09 15:33:43 +0200

    implement the required changes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { booksActions } from '../redux/books/booksSlice';
 
-const Book = ({
-  title, author, id, RemoveExistingBook,
-}) => (
-  <>
-    <h3>{title}</h3>
-    <h4>{author}</h4>
-    <button type="button">Comment</button>
-    <button type="button" onClick={() => RemoveExistingBook(id)}>Remove</button>
-    <button type="button">Edit</button>
-  </>
-);
+function Book(props) {
+  const dispatch = useDispatch();
+  const { title, author, id } = props;
+
+  const removeBookHandler = (e) => {
+    // Dispatch remove book action
+    const { id } = e.target.dataset;
+
+    dispatch(booksActions.removeBook(id));
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <span>
+        By
+        {author}
+      </span>
+      <button type="button" data-id={id} onClick={removeBookHandler}>
+        Remove
+      </button>
+    </div>
+  );
+}
 
 Book.propTypes = {
-  title: PropTypes.string,
-  author: PropTypes.string,
-  id: PropTypes.number,
-  RemoveExistingBook: PropTypes.func,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
-Book.defaultProps = {
-  title: '',
-  author: '',
-  id: '',
-  RemoveExistingBook: null,
-};
 export default Book;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import configureStore from './redux/configureStore';
-import App from './App';
 
-const store = configureStore();
+import App from './App';
+import './index.scss';
+import store from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +15,8 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
