a885b670232010322857cb37f06ebdeee44d7875
Nezerwa <eligrand2000@gmail.com>
2023-03-09 18:22:44 +0200

    connect to API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { booksActions, deleteBook } from "../redux/books/booksSlice";
 
 function Book(props) {
+  const dispatch = useDispatch();
   const { title, author, id } = props;
+
+  const removeBookHandler = (e) => {
+    // Dispatch remove book action
+    const { id } = e.target.dataset;
+    dispatch(booksActions.removeBook(id));
+
+    // Delete in the backend
+    dispatch(deleteBook(id));
+  };
+
   return (
     <div>
       <h2>{title}</h2>
-      <span>
-        By
-        {author}
-      </span>
-      <button type="button" data-id={id}>
+      <span>By {author}</span>
+      <button type="button" data-id={id} onClick={removeBookHandler}>
         Remove
       </button>
     </div>
   );
 }
-
-Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-};
 
 export default Book;
