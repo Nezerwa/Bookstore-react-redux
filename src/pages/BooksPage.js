d14b58052c311118b62e47ff8a3e96e0ca577652
Nezerwa <eligrand2000@gmail.com>
2023-03-10 14:21:34 +0200

    implement the required changes

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import Book from '../components/Book';
-import Form from '../components/Form';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+import Book from "../components/Book";
+import Form from "../components/Form";
+import { getBooks } from "../redux/books/booksSlice";
 
 function BooksPage() {
-  const books = [
-    {
-      id: 'b1',
-      title: 'The Hunger Gane',
-      author: 'Suzanne Collins',
-      category: 'action',
-    },
-    {
-      id: 'b2',
-      title: 'Dune',
-      author: 'Frank Herbert',
-      category: 'science-fiction',
-    },
-    {
-      id: 'b3',
-      title: 'Capital in the 21st Century',
-      author: 'Suzanne Collins',
-      category: 'economy',
-    },
-  ];
+  const { books, isLoading } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  let output;
+  if (isLoading) output = <h3>Loading...</h3>;
+
+  if (books?.length > 0) {
+    output = books.map((bk) => (
+      <Book
+        key={bk.item_id}
+        title={bk.title}
+        author={bk.author}
+        id={bk.item_id}
+      />
+    ));
+  }
+
+  if (!isLoading && books?.length === 0) {
+    output = <h3>No books to display...</h3>;
+  }
+
   return (
     <div>
       <div className="books-list-container row">
-        <ul className="books-list">
-          {books.map((bk) => (
-            <Book key={bk.id} title={bk.title} author={bk.author} id={bk.id} />
-          ))}
-        </ul>
+        <ul className="books-list">{output}</ul>
       </div>
       <Form />
     </div>
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,15 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const categoriesSlice = createSlice({
-  name: 'categories',
+  name: "categories",
   initialState: {
     categories: [],
   },
 
   reducers: {
-    checkStatus: (state) => state.categories.push('Under construction'),
+    checkStatus: (state) => state.categories.push("Under construction"),
   },
 });
 
-export const { addBook, removeBook } = categoriesSlice.actions;
+export const categoriesActions = categoriesSlice.actions;
 export default categoriesSlice.reducer;
