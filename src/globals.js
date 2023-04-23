17091d7b8cff9b209b9a837e41051778b7b8dab4
Nezerwa <eligrand2000@gmail.com>
2023-04-23 19:21:37 +0200

    Implement createAsyncthunks fo fetching and adding book to API

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,4 @@
-const API_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/XMpcykCejhyFfnA1ddnb';
+const API_URL =
+  "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/XMpcykCejhyFfnA1ddnb";
 
 export default API_URL;
